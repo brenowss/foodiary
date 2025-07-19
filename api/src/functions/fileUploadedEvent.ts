acfdbce9c31b80dd78d2b0ad25464a4a345a5923
brenowss <brenoc.fiorese@hotmail.com.br>
2025-07-19 20:18:37 -0300

    feat: refatorar função de processamento de eventos de upload de arquivos para enviar mensagens em lotes para o SQS

--- api/src/functions/fileUploadedEvent.ts
+++ api/src/functions/fileUploadedEvent.ts
@@ -2,20 +2,21 @@ import { SendMessageBatchCommand } from '@aws-sdk/client-sqs';
 import { S3Event } from 'aws-lambda';
 import { sqsClient } from '../clients/sqsClient';
 
-export const fileUploadedEvent = async (event: S3Event) => {
-  const entries = event.Records.map((record, index) => ({
-    Id: `message-${index}`,
-    MessageBody: JSON.stringify({
-      fileKey: record.s3.object.key,
-    }),
-  }));
+export async function handler(event: S3Event) {
+  const BATCH_SIZE = 10;
+  const records = event.Records;
 
-  if (entries.length > 0) {
+  for (let i = 0; i < records.length; i += BATCH_SIZE) {
+    const batch = records.slice(i, i + BATCH_SIZE);
+
     const command = new SendMessageBatchCommand({
       QueueUrl: process.env.MEALS_QUEUE_URL,
-      Entries: entries,
+      Entries: batch.map((record, index) => ({
+        Id: `${i + index}`,
+        MessageBody: JSON.stringify({ fileKey: record.s3.object.key }),
+      })),
     });
 
     await sqsClient.send(command);
   }
-};
+}
