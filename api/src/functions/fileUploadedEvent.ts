164df1ea7d1f4f9b19a623b40cf6cf92a8462dad
brenowss <brenoc.fiorese@hotmail.com.br>
2025-07-19 19:01:18 -0300

    feat: adicionar suporte ao AWS SQS para processamento de eventos de upload de arquivos e configurar filas no serverless.yml

--- api/src/functions/fileUploadedEvent.ts
+++ api/src/functions/fileUploadedEvent.ts
@@ -1,9 +1,21 @@
+import { SendMessageBatchCommand } from '@aws-sdk/client-sqs';
 import { S3Event } from 'aws-lambda';
+import { sqsClient } from '../clients/sqsClient';
 
 export const fileUploadedEvent = async (event: S3Event) => {
-  await Promise.all(
-    event.Records.map((record) => {
-      record.s3.object.key;
-    })
-  );
+  const entries = event.Records.map((record, index) => ({
+    Id: `message-${index}`,
+    MessageBody: JSON.stringify({
+      fileKey: record.s3.object.key,
+    }),
+  }));
+
+  if (entries.length > 0) {
+    const command = new SendMessageBatchCommand({
+      QueueUrl: process.env.MEALS_QUEUE_URL,
+      Entries: entries,
+    });
+
+    await sqsClient.send(command);
+  }
 };
